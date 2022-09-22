97e7a073739517bcdc6e890f24aa74db5240ed72
Jugal Chauhan <jugalchauhan404@gmail.com>
2022-09-22 15:16:45 +0530

    Update 10354.sql

--- Solutions/10354.sql
+++ Solutions/10354.sql
@@ -1,1 +1,25 @@
+/*
+Find the 3 most profitable companies in the entire world.
+Output the result along with the corresponding company name.
+Sort the result based on profits in descending order.
 
+forbes_global_2010_2014
+
+company: varchar
+sector: varchar
+industry: varchar
+continent: varchar
+country: varchar
+marketvalue: float
+sales: float
+profits: float
+assets: float
+rank: int
+forbeswebpage: varchar
+
+*/
+
+SELECT company, profits
+FROM forbes_global_2010_2014
+ORDER BY profits DESC
+LIMIT 3
