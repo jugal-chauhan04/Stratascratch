735f1f7b359ecc141290ee9020b854f7e1dcbc76
Jugal Chauhan <jugalchauhan404@gmail.com>
2022-09-30 16:14:43 +0530

    Update Top Ranked Songs.sql

--- Solutions/Top Ranked Songs.sql
+++ Solutions/Top Ranked Songs.sql
@@ -1,1 +1,30 @@
+/*
+Find songs that have ranked in the top position.
+Output the track name and the number of times it ranked at the top.
+Sort your records by the number of times the song was in the top position in descending order.
 
+Table: spotify_worldwide_daily_song_ranking
+
+spotify_worldwide_daily_song_ranking
+id: int
+position: int
+trackname: varchar
+artist: varchar
+streams: int
+url: varchar
+date: datetime
+region: varchar
+
+*/
+
+SELECT
+      trackname,
+      COUNT(*) AS n_top_rank
+FROM
+      spotify_worldwide_daily_song_ranking
+WHERE
+      position = 1
+GROUP BY
+      trackname
+ORDER BY 
+      n_top_rank DESC
