e250967569dbd623651ea9e5343b91e2e9ef036e
Jugal Chauhan <jugalchauhan404@gmail.com>
2022-10-14 15:22:48 +0530

    Update Highest Salary In Department.sql

--- Solutions/Highest Salary In Department.sql
+++ Solutions/Highest Salary In Department.sql
@@ -1,1 +1,29 @@
+/*
+Find the employee with the highest salary per department.
+Output the department name, employee's first name along with the corresponding salary.
 
+Table: employee
+
+employee
+id:int
+first_name:varchar
+last_name:varchar
+age:int
+sex:varchar
+employee_title:varchar
+department:varchar
+salary:int
+target:int
+bonus:int
+email:varchar
+city:varchar
+address:varchar
+manager_id:int
+*/
+
+SELECT
+      department,first_name,salary 
+FROM 
+      employee
+WHERE
+      salary IN (SELECT max(salary) FROM employee GROUP BY department)
