88fc1b927e771bc7d0f513a1ce430ac575c438b3
Jugal Chauhan <jugalchauhan404@gmail.com>
2022-10-19 10:43:04 +0530

    Update Top Cool Votes.sql

--- Solutions/Top Cool Votes.sql
+++ Solutions/Top Cool Votes.sql
@@ -1,1 +1,26 @@
+/*
+Find the review_text that received the highest number of  'cool' votes.
+Output the business name along with the review text with the highest numbef of 'cool' votes.
 
+Table: yelp_reviews
+
+yelp_reviews
+
+business_name:varchar
+review_id:varchar
+user_id:varchar
+stars:varchar
+review_date:datetime
+review_text:varchar
+funny:int
+useful:int
+cool:int
+*/
+
+SELECT
+      business_name,
+      review_text
+FROM
+      yelp_reviews
+WHERE 
+      cool IN (SELECT max(cool) from yelp_reviews)
