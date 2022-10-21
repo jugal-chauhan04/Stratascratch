720d056833b1217fa9845bcadd6f979552e34db0
Jugal Chauhan <jugalchauhan404@gmail.com>
2022-10-21 17:08:24 +0530

    Update Find the rate of processed tickets for each type.sql

--- Solutions/Find the rate of processed tickets for each type.sql
+++ Solutions/Find the rate of processed tickets for each type.sql
@@ -11,6 +11,8 @@ processed:bool
 SELECT
     type,
     SUM(CASE WHEN processed THEN 1 ELSE 0 END) :: NUMERIC / COUNT(*) AS processed_rate
-FROM facebook_complaints
-GROUP BY type
+FROM
+    facebook_complaints
+GROUP BY
+    type
 
