eb59f322534d242f7b7d8616d7238cd054f40908
Jugal Chauhan <jugalchauhan404@gmail.com>
2022-10-23 22:31:08 +0530

    Update Employee and Manager Salaries.sql

--- Solutions/Employee and Manager Salaries.sql
+++ Solutions/Employee and Manager Salaries.sql
@@ -1,1 +1,33 @@
+/*
+Find employees who are earning more than their managers.
+Output the employee's first name along with the corresponding salary.
+
+Table: employee
+
+id:int
+first_name:varchar
+last_name:varchar
+age:int
+sex:varchar
+employee_title:varchar
+department:varchar
+salary:int
+target:int
+bonus:int
+email:varchar
+city:varchar
+address:varchar
+manager_id:int
+*/
+
+SELECT 
+      a.first_name, a.salary
+FROM
+      employee a
+JOIN 
+      employee b
+ON 
+      a.manager_id = b.id
+WHERE
+      a.salary > b.salary;
 
