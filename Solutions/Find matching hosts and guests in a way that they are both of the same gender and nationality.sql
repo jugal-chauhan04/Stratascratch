518925891c15046f203a607bf6be5e352eb4b0af
Jugal Chauhan <jugalchauhan404@gmail.com>
2022-10-28 19:08:39 +0530

    Update Find matching hosts and guests in a way that they are both of the same gender and nationality.sql

--- Solutions/Find matching hosts and guests in a way that they are both of the same gender and nationality.sql
+++ Solutions/Find matching hosts and guests in a way that they are both of the same gender and nationality.sql
@@ -1,1 +1,29 @@
+/*
+Find matching hosts and guests pairs in a way that they are both of the same gender and nationality.
+Output the host id and the guest id of matched pair.
 
+Tables: airbnb_hosts, airbnb_guests
+
+airbnb_hosts
+
+host_id:int
+nationality:varchar
+gender:varchar
+age:int
+
+airbnb_guests
+
+guest_id:int
+nationality:varchar
+gender:varchar
+age:int
+*/
+
+SELECT DISTINCT 
+      host.host_id, 
+      guest.guest_id
+FROM 
+      airbnb_hosts host
+JOIN 
+      airbnb_guests guest
+USING(nationality, gender)
