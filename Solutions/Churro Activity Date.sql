5233e0002ed9b19de0961d74f485667cf18bf05d
Jugal Chauhan <jugalchauhan404@gmail.com>
2022-11-02 20:57:17 +0530

    Update Churro Activity Date.sql

--- Solutions/Churro Activity Date.sql
+++ Solutions/Churro Activity Date.sql
@@ -1,1 +1,34 @@
+/*
+Find the activity date and the pe_description of facilities with the name 'STREET CHURROS' and with a score of less than 95 points.
 
+Table: los_angeles_restaurant_health_inspections
+
+serial_number:varchar
+activity_date:datetime
+facility_name:varchar
+score:int
+grade:varchar
+service_code:int
+service_description:varchar
+employee_id:varchar
+facility_address:varchar
+facility_city:varchar
+facility_id:varchar
+facility_state:varchar
+facility_zip:varchar
+owner_id:varchar
+owner_name:varchar
+pe_description:varchar
+program_element_pe:int
+program_name:varchar
+program_status:varchar
+record_id:varchar
+*/
+
+SELECT 
+      activity_date,
+      pe_description
+FROM 
+      los_angeles_restaurant_health_inspections
+WHERE 
+      facility_name = 'STREET CHURROS' AND score < 95
