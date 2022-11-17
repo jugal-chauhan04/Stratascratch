a374fb541d0f6dc75996dfbbfef6de9004e684df
Jugal Chauhan <jugalchauhan404@gmail.com>
2022-11-17 10:42:15 +0530

    Update Average Salaries.sql

--- Solutions/Average Salaries.sql
+++ Solutions/Average Salaries.sql
@@ -1,1 +1,28 @@
+/*
+Compare each employee's salary with the average salary of the corresponding department.
+Output the department, first name, and salary of employees along with the average salary of that department.
 
+Table: employee
+
+id:int
+first_name:varchar
+last_name:varchar
+age:int
+sex:varchar
+employee_title:varchar
+department:varchar
+salary:int
+target:int
+bonus:int
+email:varchar
+city:varchar
+address:varchar
+manager_id:int
+*/
+
+SELECT department, 
+       first_name, 
+       salary, 
+       AVG(salary) over (PARTITION BY department) as average,
+       (salary - AVG(salary) over (PARTITION BY department)) as difference
+FROM employee;
