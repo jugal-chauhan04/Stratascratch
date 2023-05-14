663018823ef2cf00dddeb0a2f7b74d03c8c0bf1b
Jugal Chauhan <jugalchauhan404@gmail.com>
2023-05-14 19:24:17 -0400

    Update and rename Activity Rank to ActivityRank.sql

--- Solutions/ActivityRank.sql
+++ Solutions/ActivityRank.sql
@@ -5,5 +5,21 @@ Output the user, total emails, and their activity rank. Order records by the tot
 Sort users with the same number of emails in alphabetical order.
 In your rankings, return a unique value (i.e., a unique rank) even if multiple users have the same number of emails.
 For tie breaker use alphabetical order of the user usernames.
+
+Table: google_gmail_emails
+
+id: int
+from_user: varchar
+to_user: varchar
+day: int
+
+
+ANSWER
 */
+
+ from_user, COUNT(day),
+ROW_NUMBER() OVER(ORDER BY COUNT(day) desc, from_user asc ) AS rank
+from google_gmail_emails
+group by from_user
+order by rank asc, from_user asc
 
